01d3f971dad09349b50a515d2ede6bdd89812218
san24id <nurikhsan175@gmail.com>
2020-04-01 09:15:06 +0700

    added MVC SuperAdmin

---
+++ application/controllers/SuperAdm.php
@@ -0,0 +1,131 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class SuperAdm extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+
+
+	function __construct(){
+		parent::__construct();
+		$this->load->model('Dashboard_model');
+		$this->load->model('Home_model');
+		$this->load->model('SuperAdm_model');
+		$this->load->library('Pdf');
+
+		// $this->load->library('session');
+ 		
+ 		// if ($this->session->userdata('id_adm')) {
+ 			
+		// }else{
+		// 	redirect('login//loginadm', 'refresh');
+		// }
+	}
+
+	public function index()
+	{
+		$data['active1'] = 'active';
+		$data['active2'] = '';
+		$data['active3'] = '';
+
+		$data['profil'] = $this->SuperAdm_model->getProfilId();
+		$data['draft'] = $this->Dashboard_model->getTotalDraft();
+		$data['tot_pay_req'] = $this->Dashboard_model->getTotal();
+		$data['pembayaran'] = $this->Dashboard_model->getVPayment();
+		$data['payment'] = $this->Dashboard_model->payment();
+
+		$this->load->view('akses/superadmin/header_admin', $data);
+        $this->load->view('akses/superadmin/data_payrequest', $data);
+	}
+
+	public function addstaff(){
+		$add = array(
+
+			'display_name' => $_POST['display_name'],
+			'email' => $_POST['email'],
+			'username' =>  $_POST['username'],
+			'password' =>  $_POST['password'],
+			'role' =>  $_POST['role']
+		);
+
+		$this->SuperAdm_model->addstaff($add);
+
+		redirect('SuperAdm');
+	}
+
+	public function deletestaff(){
+
+		$this->SuperAdm_model->deletestaff($_POST['id_user']);
+
+		redirect('SuperAdm');
+
+	}
+
+	public function updatestaff(){
+		$upd = array(
+			'id_user' => $_POST['id_user'],
+			'display_name' => $_POST['display_name'],
+			'email' => $_POST['email'],
+			'username' =>  $_POST['username'],
+			'password' =>  $_POST['password'],
+			'role_id' =>  $_POST['role_id'],
+			'division_id' => $_POST['division_id'],
+			'role_status' => $_POST['role_status'],
+			'status' => $POST['status'],
+		);
+
+		$this->SuperAdm_model->updatestaff($upd);
+
+		redirect('SuperAdm');
+	}
+		
+	public function user(){
+		$data['data_user'] = 'active';
+		$data['active2'] = '';
+		$data['active3'] = '';
+
+		$data['user'] = $this->SuperAdm_model->getuser();
+		
+		$this->load->view('akses/admin/header_admin', $data);
+		$this->load->view('akses/admin/user_superadm', $data);
+	}
+
+	public function updateuser(){
+		$upd = array(
+			'id_user' => $_POST['id_user'],
+			'nomor_user' => $_POST['nomor_user'],
+			'nik' => $_POST['nik'],
+			'foto' => $_POST['foto'],
+			'nama_user' =>  $_POST['nama_user'],
+			'instansi' =>  $_POST['instansi'],
+			'jabatan' =>  $_POST['jabatan'],
+			'email' => $_POST['email'],
+			'telepon' => $_POST['telepon'],
+			'username' => $_POST['username'],			
+			'status' => $_POST['status']
+			// 'status_1' => $_POST['status_1'],
+			// 'log_create' => $_POST['log_create'],
+			// 'log_update' => $_POST['log_update'],
+			// 'last_login' => $_POST['last_login']
+		);
+
+		$this->SuperAdm_model->updateuser($upd);
+
+		redirect('SuperAdm/user');
+	}
+
+}
---
+++ application/models/SuperAdm_model.php
@@ -0,0 +1,43 @@
+ <?php
+
+class SuperAdm_model extends CI_model {
+  
+    public function getProfilId(){
+    	
+		$result = $this->db->get('m_user')->result(); 
+		return $result; 
+    }
+
+    function addstaff($add){
+        $sql = "INSERT INTO `m_user`(id_user, username, password, display_name, role_id, division_id, created_by, created_date, role_status, email, status, status_1, status_mail1, status_mail2) 
+        VALUES ('".$add['id_user']."','".$add['username']."',md5('".$add['password']."'),'".$add['display_name']."','".$add['role_id']."','".$add['division_id']."',
+        '".$add['created_by']."','".$add['created_date']."','".$add['role_status']."','".$add['email']."','".$add['status']."','".$add['status_1']."',
+        '".$add['status_mail1']."','".$add['status_mail2']."')";
+
+        $query = $this->db->query($sql);
+
+        return $query;
+    }
+    
+    public function update_myprofil($myprofil){
+        $sql = "UPDATE `m_user` SET `display_name`='".$myprofil['display_name']."',`role_id`='".$myprofil['role_id']."',`division_id`='".$myprofil['division_id']."',
+                `role_status`='".$myprofil['role_status']."',`email` = '".$myprofil['email']."', `created_date`= NOW() WHERE id_user = '".$myprofil['id_user']."'";
+
+        $query = $this->db->query($sql);
+
+        return $query;
+    }
+
+    public function update_myprofilpass($myprofil){
+        $sql = "UPDATE `m_user` SET `display_name`='".$myprofil['display_name']."',`role_id`='".$myprofil['role_id']."',`division_id`='".$myprofil['division_id']."',
+                `role_status`='".$myprofil['role_status']."',`email` = '".$myprofil['email']."', `password`=md5('".$myprofil['password_baru']."'), 
+                `created_date`= NOW() WHERE id_user = '".$myprofil['id_user']."'";
+
+        $query = $this->db->query($sql);
+
+        return $query;
+    }
+    
+    
+
+}
