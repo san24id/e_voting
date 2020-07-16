c36403b8c1176c56c53ede450e94894e042ae959
san24id <nurikhsan175@gmail.com>
2020-07-16 20:03:52 +0700

    change home model function

--- application/models/Home_model.php
+++ application/models/Home_model.php
@@ -7,7 +7,7 @@ class Home_model extends CI_Model{
         $start_date = date('Y-01-01');
         $end_date = date('Y-m-d');
 
-        $sql = "SELECT a.*,SUBSTRING_INDEX(SUBSTRING_INDEX(a.tanggal, ',', 2), ',', -1) as tanggal_new, b.jenis_pembayaran FROM t_payment as a JOIN t_pembayaran as b ON a.jenis_pembayaran = b.id_pay WHERE division_id='$dvs' and a.status<>'XXX' AND tanggal2
+        $sql = "SELECT a.*,SUBSTRING_INDEX(SUBSTRING_INDEX(a.tanggal, ',', 2), ',', -1) as tanggal_new, b.jenis_pembayaran FROM t_payment as a JOIN t_pembayaran as b ON a.jenis_pembayaran = b.id_pay WHERE division_id='$dvs' and a.status<>'99' AND tanggal2
                 BETWEEN '$start_date' AND '$end_date' ";
                 
         $query = $this->db->query($sql)->result();
