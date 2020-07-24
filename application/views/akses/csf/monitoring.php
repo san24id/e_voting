1254bb667e2dbc33961bd95c897ec4f479992ed0
san24id <nurikhsan175@gmail.com>
2020-07-24 13:54:45 +0700

    change called field on monitoring

--- application/views/akses/csf/monitoring.php
+++ application/views/akses/csf/monitoring.php
@@ -430,7 +430,7 @@
                           }
                         }  ?>
                     </td>
-                    <td><?php echo $row->tanggal_new; ?></td>
+                    <td><?php echo $row->submit_date; ?></td>
                     <td><?php echo $row->label1; ?></td>
                     <td><?php echo $row->display_name; ?></td>
                     <td>
