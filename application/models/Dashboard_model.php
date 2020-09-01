0ae98078d1002e3d90b82c83376a1b73bc383e03
san24id <nurikhsan175@gmail.com>
2020-09-01 11:54:56 +0700

    fixed function datasearch on dashboard model

--- application/models/Dashboard_model.php
+++ application/models/Dashboard_model.php
@@ -1291,7 +1291,7 @@ class Dashboard_model extends CI_Model{
 				
 				break;
 			  case "2":
-				$sql .=" and a.jenis_pembayaran like '" . $txtsearch . "' and a.status in ('0','1','2','4','5','6','7','8','9')";
+				$sql .=" and a.jenis_pembayaran like '" . $txtsearch . "' and a.status in ('0','1','2','4','5','6','7','8','9','10')";
 				break;
 			  /*case "3":
 				$sql .=" and a.nomor_surat like '%" . $txtsearch . "%'";
